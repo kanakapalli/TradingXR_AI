80661f99af38525df993bdeb1d6a4f8bd4b22582
KANAKAPALLI ANURAG <38519098+kanakapalli@users.noreply.github.com>
2024-05-25 00:26:33 +0530

    sample function and tool

---
+++ tool/functions.py
@@ -0,0 +1,26 @@
+import requests
+
+api_key = 'D9FSHNEMC8Z5AWVR'
+
+
+def fetch_news_sentiment(api_key, tickers=None, topics=None, time_from=None, time_to=None, sort='LATEST', limit=50):
+    base_url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT'
+    params = {
+        'apikey': api_key,
+        'tickers': tickers,
+        'topics': topics,
+        'time_from': time_from,
+        'time_to': time_to,
+        'sort': sort,
+        'limit': limit
+    }
+    
+    # Remove None values from params
+    params = {k: v for k, v in params.items() if v is not None}
+    
+    response = requests.get(base_url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {'error': f'Error {response.status_code}: {response.text}'}
+
